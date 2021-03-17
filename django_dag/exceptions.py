2c52e306bbbe88c88d8707b851bbba0b1ea57698
Paul Gammans <pgammans@gammascience.co.uk>
2021-03-17 14:49:34 +0000

    Add more exception types

--- django_dag/exceptions.py
+++ django_dag/exceptions.py
@@ -1,6 +1,12 @@
 
 class NodeNotReachableException (Exception):
-    """
-    Exception for node distance and path
-    """
+    """Exception for node distance and path"""
+    pass
+
+class InvalidNodeMove (Exception):
+    """Invalid Node Movement"""
+    pass
+
+class InvalidNodeInsert (Exception):
+    """Invalid Node Insert"""
     pass
